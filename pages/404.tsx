3475cb92830797343c5ccdf0230701e4a044bd17
Fico Seitún <fseitun@gmail.com>
2022-07-04 19:52:11 -0300

    Add 404

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import { Container } from '@mui/material';
+
+export default function Custom404() {
+  return (
+    <Container style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      Try the following user id: dc438d87-18fc-4f65-8927-d860d1496795
+    </Container>
+  );
+}
