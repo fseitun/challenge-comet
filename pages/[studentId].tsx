3b8ec8f03343beba7d817e3861bc85baccc33f6d
Fico Seitún <fseitun@gmail.com>
2022-07-04 18:24:59 -0300

    Change next's API to recieve param

---
+++ pages/[studentId].tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
+import { Fees } from '../components/Fees';
+import { Status } from '../components/Status';
+import { useStudents } from '../utils/useStudents';
+import { useOrders } from '../utils/useOrders';
+import { Student, Order } from '../types';
+
+//3b35fb50-3d5e-41b3-96d6-c5566141fab0
+
+const Home: NextPage = () => {
+  const [total, setTotal] = useState(0);
+
+  const router = useRouter();
+  const { studentId } = router.query;
+
+  const selecterdStudent: Student | null = useStudents(studentId as string);
+  const studentOrders: Order[] | null = useOrders(studentId as string);
+
+  return (
+    <>
+      <Head>
+        <title>Cometa School App</title>
+        <meta name="Cometa School App" content="Cometa School App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Status student={selecterdStudent} totalToPay={total} />
+          ------------------------------------------
+          <Fees type={'PAID'} orders={studentOrders} />
+          ------------------------------------------
+          <Fees type={'DUE'} orders={studentOrders} setTotal={setTotal} />
+          ------------------------------------------
+          <Fees type={'OUTSTANDING'} orders={studentOrders} setTotal={setTotal} />
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default Home;
--- pages/api/students/[sid]/index.ts
+++ pages/api/students/[sid]/index.ts
@@ -3,31 +3,33 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { Student } from '../../../../types';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Student>) {
-  res.status(200).json({
-    id: '3b35fb50-3d5e-41b3-96d6-c5566141fab0',
-    first_name: 'Mateo',
-    last_name: 'Creamer',
-    guardian: {
-      id: 'b5ad0bde-f1bb-433a-8124-10379cc906f5',
-      first_name: 'Valeria',
-      last_name: 'Wu',
-      email: 'adoval4@gmail.com',
-      phone: '+51950862507',
-      tax_id: '47865675',
-    },
-    cohort: 'Primer año',
-    school: {
-      id: '41ecfd5e-ffd2-44b3-8df5-70279624ad41',
-      name: 'Innova Schools San Miguel',
-      logo: null,
-      country: 'PE',
-      city: null,
-      address: 'Jr. La paz 123',
-      zip_code: null,
-    },
-    monthly_grant_type: null,
-    monthly_grant_value: null,
-    inscription_grant_value: null,
-    inscription_grant_type: null,
-  });
+  if (req.method === 'GET' && req.query.sid === '3b35fb50-3d5e-41b3-96d6-c5566141fab0') {
+    res.status(200).json({
+      id: '3b35fb50-3d5e-41b3-96d6-c5566141fab0',
+      first_name: 'Mateo',
+      last_name: 'Creamer',
+      guardian: {
+        id: 'b5ad0bde-f1bb-433a-8124-10379cc906f5',
+        first_name: 'Valeria',
+        last_name: 'Wu',
+        email: 'adoval4@gmail.com',
+        phone: '+51950862507',
+        tax_id: '47865675',
+      },
+      cohort: 'Primer año',
+      school: {
+        id: '41ecfd5e-ffd2-44b3-8df5-70279624ad41',
+        name: 'Innova Schools San Miguel',
+        logo: null,
+        country: 'PE',
+        city: null,
+        address: 'Jr. La paz 123',
+        zip_code: null,
+      },
+      monthly_grant_type: null,
+      monthly_grant_value: null,
+      inscription_grant_value: null,
+      inscription_grant_type: null,
+    });
+  }
 }
