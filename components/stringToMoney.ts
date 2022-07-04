dddbf2883a3a1fb0851fe2f17dfd11aa1750b9ae
Fico Seitún <fseitun@gmail.com>
2022-07-04 17:42:43 -0300

    Export stringToMoney function as module

---
+++ components/stringToMoney.ts
@@ -0,0 +1,9 @@
+export function stringToMoney(string: string) {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 0,
+  });
+
+  return formatter.format(Number(string));
+}
