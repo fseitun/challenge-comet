26781befacb95b75ed416d5c442834f6718e956f
Fico Seitún <fseitun@gmail.com>
2022-07-04 20:42:04 -0300

    Set 404 base url dynamically

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react';
 import { Container } from '@mui/material';
 
 export default function Custom404() {
+  const [environmentUrl, setEnvironmentUrl] = useState('');
+
+  useEffect(() => {
+    setEnvironmentUrl(`${window.location.href}dc438d87-18fc-4f65-8927-d860d1496795`);
+  }, []);
+
   return (
     <Container style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      Try the following user id: dc438d87-18fc-4f65-8927-d860d1496795
+      Try
+      <a rel="noopener noreferrer" href={environmentUrl}>
+        &nbsp;the following&nbsp;
+      </a>
+      user id
     </Container>
   );
 }
