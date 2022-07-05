d936d1d97c771d3eca8431145624b77d57f3d3b0
Fico Seitún <fseitun@gmail.com>
2022-07-04 21:18:11 -0300

    Change API url from https to http

--- utils/useOrders.tsx
+++ utils/useOrders.tsx
@@ -6,7 +6,7 @@ export function useOrders(studentId: string): Order[] | null {
   const [orders, setOrders] = useState<Order[] | null>(null);
 
   useEffect(() => {
-    fetch(`https://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/${studentId}/orders`, {
+    fetch(`http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/${studentId}/orders`, {
       method: 'GET',
       headers: {
         hash: 'OcJn4jYChW',
