9a9198a07a8b694d0c7e8d820a0ce0d14d1e7df5
Fico Seitún <fseitun@gmail.com>
2022-07-04 21:06:22 -0300

    Change Next API for AWS

--- utils/useOrders.tsx
+++ utils/useOrders.tsx
@@ -6,10 +6,21 @@ export function useOrders(studentId: string): Order[] | null {
   const [orders, setOrders] = useState<Order[] | null>(null);
 
   useEffect(() => {
-    fetch(`/api/students/${studentId}/orders`)
+    fetch(`http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/${studentId}/orders`, {
+      method: 'GET',
+      headers: {
+        hash: 'OcJn4jYChW',
+      },
+    })
       .then(response => response.json())
       .then(data => setOrders(data));
   }, [studentId]);
 
   return orders;
 }
+
+// useEffect(() => {
+//   fetch(`/api/students/${studentId}/orders`)
+//     .then(response => response.json())
+//     .then(data => setOrders(data));
+// }, [studentId]);
--- utils/useStudents.tsx
+++ utils/useStudents.tsx
@@ -5,11 +5,29 @@ import { Student } from '../types';
 export function useStudents(studentId: string): Student | null {
   const [student, setStudent] = useState<Student | null>(null);
 
+  const myHeaders = new Headers();
+
+  const myRequest = new Request('flowers.jpg', {
+    method: 'GET',
+    headers: myHeaders,
+  });
+
   useEffect(() => {
-    fetch(`/api/students/${studentId}`)
+    fetch(`http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/${studentId}`, {
+      method: 'GET',
+      headers: {
+        hash: 'OcJn4jYChW',
+      },
+    })
       .then(response => response.json())
       .then(data => setStudent(data));
   }, [studentId]);
 
   return student;
 }
+
+// useEffect(() => {
+//   fetch(`/api/students/${studentId}`)
+//     .then(response => response.json())
+//     .then(data => setStudent(data));
+// }, [studentId]);
