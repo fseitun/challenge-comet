2dad81f9ea38f40516050fe6c44e30f18fa93396
Fico Seitún <fseitun@gmail.com>
2022-07-05 08:48:17 -0300

    Restore next mocked api

--- utils/useOrders.tsx
+++ utils/useOrders.tsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
+import https from 'https';
 
 import { Order } from '../types';
+
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
 export function useOrders(studentId: string): Order[] | null {
   const [orders, setOrders] = useState<Order[] | null>(null);
 
   useEffect(() => {
-    fetch(`http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/${studentId}/orders`, {
-      method: 'GET',
-      headers: {
-        hash: 'OcJn4jYChW',
-      },
-    })
+    fetch(`/api/students/${studentId}/orders`)
       .then(response => response.json())
       .then(data => setOrders(data));
   }, [studentId]);
@@ -20,7 +21,15 @@ export function useOrders(studentId: string): Order[] | null {
 }
 
 // useEffect(() => {
-//   fetch(`/api/students/${studentId}/orders`)
-//     .then(response => response.json())
+//   axios
+//     .get(`http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/${studentId}/orders`, {
+//       httpsAgent,
+//       headers: {
+//         // 'Content-Type': 'application/json',
+//         // Accept: 'application/json',
+//         hash: 'OcJn4jYChW',
+//         'Access-Control-Allow-Origin': '*',
+//       },
+//     })
 //     .then(data => setOrders(data));
 // }, [studentId]);
--- utils/useStudents.tsx
+++ utils/useStudents.tsx
@@ -1,24 +1,18 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
+import https from 'https';
 
 import { Student } from '../types';
+
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
 export function useStudents(studentId: string): Student | null {
   const [student, setStudent] = useState<Student | null>(null);
 
-  const myHeaders = new Headers();
-
-  const myRequest = new Request('flowers.jpg', {
-    method: 'GET',
-    headers: myHeaders,
-  });
-
   useEffect(() => {
-    fetch(`http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/${studentId}`, {
-      method: 'GET',
-      headers: {
-        hash: 'OcJn4jYChW',
-      },
-    })
+    fetch(`/api/students/${studentId}`)
       .then(response => response.json())
       .then(data => setStudent(data));
   }, [studentId]);
@@ -26,8 +20,22 @@ export function useStudents(studentId: string): Student | null {
   return student;
 }
 
+/**
+ * headers: {
+ hash: 'OcJn4jYChW',
+},
+*/
+
 // useEffect(() => {
-//   fetch(`/api/students/${studentId}`)
-//     .then(response => response.json())
+//   axios
+//     .get(`http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/${studentId}`, {
+//       httpsAgent,
+//       headers: {
+//         // 'Content-Type': 'application/json',
+//         // Accept: 'application/json',
+//         hash: 'OcJn4jYChW',
+//         'Access-Control-Allow-Origin': '*',
+//       },
+//     })
 //     .then(data => setStudent(data));
 // }, [studentId]);
